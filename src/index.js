2d4df60362d488f86ad354daf9cf927ba7e5ae39
박경현 <rudgus4620@gmail.com>
2022-11-26 22:35:16 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+//react, react-dom 17.0.2 으로 버전 낮췄을 시, 
+//실행을 위해 수정해준 코드
+
+import React,{ StrictMode }  from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Amplify} from 'aws-amplify';
+import config from './aws-exports';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+Amplify.configure(config);
+
+ReactDOM.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
